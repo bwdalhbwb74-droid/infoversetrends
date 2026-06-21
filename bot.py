cfa9798df7005d091dc6e65cdf1cb9da7176a3a7
bwdalhbwb74-droid <bwdalhbwb74@gmail.com>
2026-06-21 09:11:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,28 @@
+from pytrends.request import TrendReq
 import requests
 
 TOKEN = "8763023216:AAGTxJFJD2dnMtBHirSdx_fMhpyszuOkmS0"
 CHAT_ID = "7330431242"
 
-message = "🚀 تم تشغيل InfoVerse Trends Bot بنجاح"
+try:
+    pytrends = TrendReq(hl='en-US', tz=360)
 
-url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+    trending_en = pytrends.trending_searches(pn='united_states')[0].head(5).tolist()
 
-requests.post(url, data={
-    "chat_id": CHAT_ID,
-    "text": message
-})
+    message = "🌍 5 English Trends\n\n"
 
-print("Message Sent")
+    for i, trend in enumerate(trending_en, 1):
+        message += f"{i}- {trend}\n"
+
+    requests.post(
+        f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+        data={
+            "chat_id": CHAT_ID,
+            "text": message
+        }
+    )
+
+    print("Trends sent")
+
+except Exception as e:
+    print(e)
