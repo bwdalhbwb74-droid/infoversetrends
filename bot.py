b00a8518ebd843e6109a9901516cb66fcbc432a0
bwdalhbwb74-droid <bwdalhbwb74@gmail.com>
2026-06-21 09:06:03 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
+import requests
 
+TOKEN = "8763023216:AAGTxJFJD2dnMtBHirSdx_fMhpyszuOkmS0"
+CHAT_ID = "7330431242"
+
+message = "🚀 تم تشغيل InfoVerse Trends Bot بنجاح"
+
+url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+requests.post(url, data={
+    "chat_id": CHAT_ID,
+    "text": message
+})
+
+print("Message Sent")
