b80fa250bbec0701a1aed5c253c5d4b59a2955c0
infoversehub <bwdalhbwb74@gmail.com>
2026-06-27 02:10:55 +0300

    Update storage.py

--- storage.py
+++ storage.py
@@ -1,1 +1,46 @@
+import json
+import os
 
+
+def load_json(file_path, default=None):
+    """
+    Load JSON file.
+    If file doesn't exist, create it using default.
+    """
+
+    if default is None:
+        default = {}
+
+    if not os.path.exists(file_path):
+        save_json(file_path, default)
+        return default
+
+    try:
+        with open(file_path, "r", encoding="utf-8") as f:
+            return json.load(f)
+    except Exception:
+        save_json(file_path, default)
+        return default
+
+
+def save_json(file_path, data):
+    """
+    Save data to JSON file.
+    """
+
+    with open(file_path, "w", encoding="utf-8") as f:
+        json.dump(
+            data,
+            f,
+            ensure_ascii=False,
+            indent=4
+        )
+
+
+def file_exists(file_path):
+    return os.path.exists(file_path)
+
+
+def delete_file(file_path):
+    if os.path.exists(file_path):
+        os.remove(file_path)
